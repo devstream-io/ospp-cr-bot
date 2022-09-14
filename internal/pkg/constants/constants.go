bbdfa8a669bf098ee3b0c8082765a943a4483c59
xiaoliangyu <lyle@hdu.edu.cn>
2022-09-14 19:29:41 +0800

    delete test codes
    feat: delete test codes
    Signed-off-by: xiaoliangyu <lyle@hdu.edu.cn>

--- internal/pkg/constants/constants.go
+++ internal/pkg/constants/constants.go
@@ -11,16 +11,9 @@ const (
 	Unread3 MsgType = 3
 )
 
-//const (
-//	TimeUnread1   = 30 * time.Minute
-//	TimeUnread2   = 30 * time.Minute
-//	TimeUnread3   = 11 * time.Hour
-//	CommentUnread = 1 * time.Hour
-//)
-
 const (
-	TimeUnread1   = 3 * time.Second
-	TimeUnread2   = 3 * time.Second
-	TimeUnread3   = 10 * time.Second
-	CommentUnread = 20 * time.Second
+	TimeUnread1   = 30 * time.Minute // PR/Issue 消息第一次发送消息后若未读，经过 TimeUnread1 后重发
+	TimeUnread2   = 30 * time.Minute // PR/Issue 消息第二次发送消息后若未读，经过 TimeUnread2 后发送给上级
+	TimeUnread3   = 11 * time.Hour   // PR/Issue 消息第三次发送消息后若未读，经过 TimeUnread3 后抄送群聊
+	CommentUnread = 1 * time.Hour    // Comment 消息第一次发送后若未读，经过 CommentUnread 后抄送群聊
 )
